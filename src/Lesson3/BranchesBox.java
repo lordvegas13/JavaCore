895537bfdfc5b94db63f76e8272497ef8d4f5ef1
lordvegas13 <whelenadraw@gmail.com>
2021-11-06 12:30:09 +0300

    Lesson3

---
+++ src/Lesson3/BranchesBox.java
@@ -0,0 +1,10 @@
+package Lesson3;
+
+public class BranchesBox extends Materials {       // доски наследуются от материалов (2)
+
+    public BranchesBox() {                         // реализуем конструктор, указываем вес 20 (3)
+        super(20);
+    }
+
+
+}
